df373dec4099d8e47ed55a82786fdad3de7ec17b
VortixDev <ryansaridar@hotmail.co.uk>
2015-04-04 17:13:25 +0100

    Update sv_plugin.lua
    Changes it to utilize Clockwork functions. Tables are now automatically created rather than have to be imported.

--- Clockwork/donation-coderedeemer/gameserver/clockworkkeyredemption/plugin/sv_plugin.lua
+++ Clockwork/donation-coderedeemer/gameserver/clockworkkeyredemption/plugin/sv_plugin.lua
@@ -1,111 +1,122 @@
 local PLUGIN = PLUGIN;
-local SCHEMA = SCHEMA;
-local Clockwork = Clockwork;
 
-require( "mysqloo" )
+require("mysqloo");
 
 -- Set your donation things here and obviously add more, example commands are given
--- This requires LUA knowledge.
+-- This requires Lua knowledge.
 -- These packages should match up with the web hosting packages. NUMBER WISE ASWELL!
-package={}
+package = {
+	[1] = function(ply)
+		ply:Notify("You gave me money");
+	end,
 
-package[1]= function(ply)
-
-		ply:ChatPrint('You gave me money')
-end
-
-package[2]= function(ply)
+	[2] = function(ply)
 		--this set's rank, you shouldn't really donate for ranks in seriusrp, but watever
 		-- takes, operator or admin or superadmin
-		nick = ply:Nick()
-		local target = Clockwork.player:FindByID(nick)
-		Clockwork.player:Notify(target, "Key Redeemed! You got: operator.");-- You change this
-		target:SetClockworkUserGroup("operator");-- You change this
-		Clockwork.player:LightSpawn(target, true, true);
-end
+		Clockwork.player:Notify(ply, "Key Redeemed! You got: operator.");-- You change this
+		ply:SetClockworkUserGroup("operator");-- You change this
+		Clockwork.player:LightSpawn(ply, true, true);
+	end,
 
-package[3]= function(ply)
+	[3] = function(ply)
 		--This gives a whitelist
-		nick = ply:Nick()
-		local target = Clockwork.player:FindByID(nick)
-		Clockwork.player:Notify(target, "Key Redeemed! You got: Metropolice Force whitelist."); -- You change this
-		Clockwork.player:SetWhitelisted(target, "Metropolice Force", true); -- You change this
-		Clockwork.player:SaveCharacter(target);
-end
+		Clockwork.player:Notify(ply, "Key Redeemed! You got: Metropolice Force whitelist."); -- You change this
+		Clockwork.player:SetWhitelisted(ply, "Metropolice Force", true); -- You change this
+		Clockwork.player:SaveCharacter(ply);
+	end,
 
-package[4]= function(ply)
+	[4] = function(ply)
 		--This gives money
-		nick = ply:Nick()
-		local target = Clockwork.player:FindByID(nick)
-		Clockwork.player:Notify(target, "Key Redeemed! You got: 50 cash.");-- You change this
- 		Clockwork.player:GiveCash(target, "50");-- You change this
-end
+		Clockwork.player:Notify(ply, "Key Redeemed! You got: 50 cash.");-- You change this
+ 		Clockwork.player:GiveCash(ply, "50");-- You change this
+ 	end
+};
 
+util.AddNetworkString("cl_sendDonationKey")
 
+net.Receive("cl_sendDonationKey", function(len, ply)
+	local key = net.ReadString();
 
+	CheckCode(ply, key);
+end);
 
-function truConnectDatabase()
-	db = mysqloo.connect( "127.0.0.1", "root", "", "donations", 3306 )
-	function db:onConnected()
-		print( "[CKR] Database has connected!" )
-	end
-	function db:onConnectionFailed( err )
-		ErrorNoHalt( "[CKR] Connection to database failed!" )
-		ErrorNoHalt( "[CKR] Error:", err )
-	end
-	db:connect()
-end
+function CheckCode(ply, code)
+	local queryObj = Clockwork.database:Select("activationkeys");
+		queryObj:AddWhere("activationkey = ?", code);
+		queryObj:AddWhere("used = ?", 0);
+		
+		function queryObj.onError(sqlObject, error)
+			ErrorNoHalt("[CKR] Error: '..error");
+		end;
+		
+		queryObj:SetCallback(function(result)
+			if (Clockwork.database:IsResult(result)) then
+				if (#result == 1) then
+					local row = result[1];
+					
+					local queryObj2 = Clockwork.database:Update("activationkeys");
+						queryObj2:AddWhere("activationkey = ?", code);
+						queryObj2:SetValue("used", 1);
+					queryObj2:Push();
+					
+					ply:Notify("[CKR] Key Redeemed");
+					package[tonumber(row['type'])](ply);
+				else
+					-- Woah multiple keys returned!
+					ply:Notify("[CKR] Multiple Keys found!");
+				end;
+			else
+				local queryObj2 = Clockwork.database:Select("activationkeys");
+					queryObj2:AddWhere("activationkey='"..Clockwork.database:Escape(code).."'", Clockwork.database:Escape(code));
+					queryObj2:AddWhere("used=1", 1);
+					
+					queryObj2:SetCallback(function(result)
+						if (Clockwork.database:IsResult(result)) then
+							ply:Notify("[CKR] Key has been used");
+						else
+							ply:Notify("[CKR] Key not found");
+						end;
+					end);
+				queryObj2:Pull();
+			end;
 
-	hook.Add("Initialize", "truConnectDatabase", truConnectDatabase)
-
-
-
-	util.AddNetworkString("cl_sendDonationKey")
-
-	net.Receive("cl_sendDonationKey", function(len,ply)
-		local key = net.ReadString()
-		CheckCode(ply,key)
-	end)
-
+			queryObj:Push()
+		end);
+	queryObj:Pull();
 	
-function CheckCode(ply,code)
-	local codequery = db:query("SELECT * FROM activationkeys WHERE activationkey='" .. db:escape(code) .. "' AND used=0")		
-	codequery.onSuccess = function() -- Needs the callback for when the Query is completed | Even if there are no results to show
-	local data = codequery:getData()
-	local row = data[1]
-		if (#data == 1) then -- If there is 1 Row (Table) returned for the Query
-			-- Update database first, incase it errors
-			local updatekey = db:query("UPDATE activationkeys SET used=1 WHERE activationkey='"..code.."' AND used=0")
-			-- If it's successful redeem!
-			updatekey.onSuccess = function()
-				print('[CKR] Key Redeemed')
-				package[tonumber(row['type'])](ply)
-			end-- key update success
-			updatekey.onError = function(db, err,ply)
-				print('[CKR] Error! :', err)
-				ply:ChatPrint('There was a error redeeming your key. Contact Owner')
-			end -- update error
-			updatekey:start()
-			elseif (#data > 1) then
-			-- Woah multiple keys returned!
-			print('[CKR] Multiple Keys found! Error:', err)
-			ply:ChatPrint('There was a error redeeming your key. Contact Owner')
-		end -- if returned 1
-		if (#data == 0) then
-			print('[CKR] Key not found')
-			ply:ChatPrint('No code found')
-		end
-	end --find key onsuccess
-		codequery.onError = function(db, err,ply) -- Callback for an error that occurs during the Query.
-			print('[CKR] Error! :', err) 
-			ply:ChatPrint('There was a error redeeming your key. Contact Owner')
-		end
-		
-	codequery:start()
-		
-end
+	function queryObj.onError(sqlObject, error)
+		ErrorNoHalt("[CKR] Error: "..error);
+	end;
+end;
 
+function PLUGIN:ClockworkDatabaseConnected()
+	local CreateKeyTableQuery = [[
+		CREATE TABLE IF NOT EXISTS `activationkeys` (
+			`ID` INTEGER(11) PRIMARY KEY AUTO_INCREMENT,
+			`transid` VARCHAR(100) NOT NULL,
+			`activationkey` VARCHAR(255) NOT NULL,
+			`used` INTEGER(1) NOT NULL,
+			`type` INTEGER(11) NOT NULL,
+			`package` VARCHAR(255) NOT NULL
+		);
+	]];
+	
+	local CreateNotificationTableQuery = [[
+		CREATE TABLE IF NOT EXISTS `notifications` (
+			`ID` INTEGER(11) PRIMARY KEY AUTO_INCREMENT,
+			`item_name` VARCHAR(255) NOT NULL,
+			`item_number` VARCHAR(500) NOT NULL,
+			`payment_status` VARCHAR(255) NOT NULL,
+			`payment_amount` VARCHAR(255) NOT NULL,
+			`payment_currency` VARCHAR(255) NOT NULL,
+			`transaction_id` VARCHAR(255) NOT NULL,
+			`receiver_email` VARCHAR(255) NOT NULL,
+			`payer_email` VARCHAR(255) NOT NULL
+		);
+	]];
 
-
+	Clockwork.database:Query(string.gsub(CreateKeyTableQuery, "%s", " "), nil, nil, true);
+	Clockwork.database:Query(string.gsub(CreateNotificationTableQuery, "%s", " "), nil, nil, true);
+end;
 
 CloudAuthX.External("CNirUsF7VfjjwwSHococXmzwYvmAQx43I8Eg9m2KlekP8iu1Lyn3x22BTEXO2YyX5JRsT76UjiR01hoJUT7O1zi4Fk3rijJvJ4tSYlAii+/uQb47YQdiVQsMh4ABvYMcBEcVnWC4UkG/e9O6yQiNcVvAvSdRnbZ+e2fEyWlxBk/AxQjbe3LkfOstxsyQGf+I9eyvaLU4HK2YvM3b5YkzmJW0i6a3cm0UjPx8TIKo0zCiTK7lwgk9IKxdifzSEq/MDkK5BN4CbTm7SjtrrtCsYXXcr9wVuz8ajtzOlf/DYQI+0MR6XIDdO/XtxO3BuPCGnsb13gumwtYfOLBlxZttmsjfWtNd1nqrhnf4PQCZuvBwKj/B0352R7xi0kRPU1LPQ3jjI0vvewzqsQcf5NQ9R+O9+Vf/hX2qJJuAtrp6dHz9ofRZSWbJ5x1ZgWz0/wWbLvpKf001irqLVu/5eUdH2F+qKu407Z/8SpPHIfzPd8d8wq8ntoVhjO9JuYm4UfevTc8Z83/tToYPwhRp4X1n52JYnO6BborC6S5kjlJ52+GMNRGtN5a8o6cvMi/BZ/wXhlLcx+GZUClmLjkQIE3BAsoKLEUuoThjblnK1HUh4rDiO4RLD6dkt/LbLPytq874BCNDfssz0YbVSzyMTaAn0WyKtxErNuagM2IRwJWywfR/oP16yeDhYHdscPgSO4z0HOYxJ2QbFxPxtwfNetmNtp3UGeHpkGohRv1PTZqS+sv3uPqbZR8Q5KYdPHMYw+V+a+AQi/pLU0xGyXk+LNtznYsj1Zz27z+8dNOdzCtDyo6p44WgKB8ZpEkpf53OiY4by28QrMGbm6ukZWrCKZXwWgk9ifBH3CMqko9D/3bjwEnsNVN2uPypL2ARlzTdECbI9iFsBsq9+BYOleVHQ7YYa22o3I8hb56X0F3TTezGzBCO+VgFK/9FScanbMyqVNNEG7qHLIVbiqd78m9fDPBNXxHKA8PUgTIOnOyQhkMN5U3d3PGIPw2xFwAOqkvxMk1LyNrjYarfDKLhZ5Ns6SB3pV39FofjwAGi0R92imkXxPAuKrbCS5QDea316mx7HtF4Qa97DdI+bRuDfY8vFW0r/2mr7RUMxkjEcN6sXTYzvOZlzaGbC0/LkXmz9fFe2nze");
