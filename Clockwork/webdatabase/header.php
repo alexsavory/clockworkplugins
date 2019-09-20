7032f165c87396089ed8913d0a2c378e14596756
Alex <trurascalz@users.noreply.github.com>
2019-09-20 08:50:42 +0100

    Update header.php
    Removed php debug

--- Clockwork/webdatabase/header.php
+++ Clockwork/webdatabase/header.php
@@ -1,7 +1,5 @@
 <?php
-      ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+      
 require 'steamauth/steamauth.php';
 include("include/config.php");
 include("include/func.php");
