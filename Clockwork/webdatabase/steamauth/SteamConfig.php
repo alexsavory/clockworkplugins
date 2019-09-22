e4d4918056166fe523fe0d334d17ee1796f7c399
Alex <trurascalz@users.noreply.github.com>
2019-09-22 15:06:46 +0100

    Update SteamConfig.php
    removed information

--- Clockwork/webdatabase/steamauth/SteamConfig.php
+++ Clockwork/webdatabase/steamauth/SteamConfig.php
@@ -1,7 +1,7 @@
 <?php
 //Version 4.0
-$steamauth['apikey'] = "406E1C8633660B090E31DE99A588E12D"; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
-$steamauth['domainname'] = "test.alexsavory.me"; // The main URL of your website displayed in the login page
+$steamauth['apikey'] = "APIKEY"; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
+$steamauth['domainname'] = "WEBSITEURL"; // The main URL of your website displayed in the login page
 $steamauth['logoutpage'] = "index.php"; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 $steamauth['loginpage'] = "profile.php"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 
